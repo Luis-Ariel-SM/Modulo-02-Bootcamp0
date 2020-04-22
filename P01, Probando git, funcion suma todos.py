429492b7e10d6e422af83c5fda77e97cb2a5f7f5
Luis Ariel Santander Méndez <luisarielsantander@gmail.com>
2020-04-22 21:45:49 +0200

    Funciones clasicas

--- P01, Probando git, funcion suma todos.py
+++ P01, Probando git, funcion suma todos.py
@@ -2,8 +2,16 @@ def suma_todos(limit_to):
     resultado = 0
     for i in range (0, limit_to+1):
         resultado += i
-        print (resultado)
-        
+                
     return resultado
 
+
+def suma_todos_cuadrados (limit_to):
+    result = 0
+    for i in range (limit_to+1):
+        result+=i*i
+        
+    return result
+
 print (suma_todos(100))
+print (suma_todos_cuadrados(3))
